601a2008d3c3bbc04d4aff24d771094cec89f18b
FIRST_NAME LAST_NAME <ashikjoseph17@gmail.com>
2024-04-29 16:02:47 +0530

    bookstore project added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  allBooks:any=[];
+  constructor(private api:ApiService){}
   title = 'bookstore-user';
+  ngOnInit(): void {
+    this.getAllListedBooks()
+  }
+  getAllListedBooks(){
+    this.api.getAllBooksApi().subscribe({
+      next:(res:any)=>{
+        console.log("ALL BOOKS")
+        console.log(res)
+        this.allBooks=res;
+      },
+      error:(res:any)=>{
+        console.log(res)
+      }
+    })
+  }
 }
